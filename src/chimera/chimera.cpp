47528e4ba9ca2cf7091817abeb9f6104dde5e8cd
MangoFizz <ccs81.mr@gmail.com>
2025-09-18 06:13:53 -0600

    Set up custom console by default, restore cls command

--- src/chimera/chimera.cpp
+++ src/chimera/chimera.cpp
@@ -594,15 +594,8 @@ namespace Chimera {
             // Load the custom chat
             initialize_custom_chat();
 
-            // If we're using a custom console, set that up
-            if(chimera->get_ini()->get_value_bool("custom_console.enabled").value_or(false)) {
-                setup_custom_console();
-            }
-            // Otherwise, merely fix console fade
-            else {
-                // Fix console fade
-                setup_console_fade_fix();
-            }
+            // Always set up the custom console
+            setup_custom_console();
 
             // Fix the death reset time
             setup_death_reset_time_fix();
--- src/chimera/console/console.cpp
+++ src/chimera/console/console.cpp
@@ -195,6 +195,18 @@ namespace Chimera {
 
     // This function intercepts the console and reads the buffer.
     static void read_command() {
+        // If "clear" is sent to the console, change it to "cls"
+        if(std::strcmp(console_text, "clear") == 0) {
+            std::strcpy(console_text, "cls");
+        }
+
+        // Clear the text if needed
+        if(std::strcmp(console_text, "cls") == 0) {
+            position = 0;
+            custom_lines.clear();
+            return;
+        }
+
         // If this is the rcon command, we may need to do special things
         if(std::strncmp(console_text, "rcon", 4) == 0 && get_chimera().feature_present("client") && server_type() == ServerType::SERVER_DEDICATED) {
             rcon_command_used_recently = true;
